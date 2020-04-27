5a242594c0ae4b714ac33070ee5daa0b5eb4225c
kristenmoran <kristen.moran30@gmail.com>
2020-04-27 14:52:26 -0400

    footer added with styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+	return (
+		<div>
+			<footer className='footerContainer'>
+				<p className='contactInfo'>Made by Kristen Moran</p>
+			</footer>
+		</div>
+	);
+}
+
+export default Footer;
