57b62c337ea00c879744e015c7d258d7c1a891c3
kristenmoran <kristen.moran30@gmail.com>
2020-04-27 13:01:18 -0400

    nav bar added with stying to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+import Logo from '../../imgs/studioghiblilogo.png';
 
 function Header() {
-	return <div>Hello from the Header</div>;
+	return (
+		<header className='container'>
+			<nav className='navContainer'>
+				<Link className='homepage' to=''>
+					Home
+				</Link>
+				<Link className='moviesButton' to=''>
+					Movies
+				</Link>
+				<Link className='contact' to='_blank'>
+					Contact
+				</Link>
+			</nav>
+				<img className='studioGhibliLogo' src={Logo} alt='Studio Ghibli Logo' />
+		</header>
+	);
 }
 
 export default Header;
