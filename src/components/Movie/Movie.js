b6de6e000bb23e0107ee776a58992efd21955737
kristenmoran <kristen.moran30@gmail.com>
2020-04-28 22:51:23 -0400

    worked on getting film content to load dynamically

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,27 +1,35 @@
 import React from 'react';
 
 function Movie({ films }) {
+	// if (films.length === 0) {
+	// 	return <div className='loadingMovie'>Loading Movie</div>;
+	// }
+
+	// let movie = this.films.find((movie) => movie.title === this.match.title)
+	// console.log(movie)
+
 	return (
 		<div className='movieContainer'>
-			{/* {films.map((movie) => (
-				<section key={movie.id} className='descriptionContainer'>
-					<h1 key={movie.id} className='movieTitle'>
-						{movie.title}
-					</h1>
-					<h3 key={movie.id} className='releaseDate'>
-						{movie.release_date}
-					</h3>
-					<h3 key={movie.id} className='director'>
-						{movie.director}
-					</h3>
-					<p key={movie.id} className='movieDescription'>
-						{movie.description}
-					</p>
-					<h6 key={movie.id} className='rtScore'>
-						{movie.rt_score}
-					</h6>
-				</section>
-			))} */}
+			{/* {films &&
+				films.map((movie) => (
+					<section key={movie.id} className='descriptionContainer'>
+						<h1 key={movie.id} className='movieTitle'>
+							{movie.title}
+						</h1>
+						<h3 key={movie.id} className='releaseDate'>
+							{movie.release_date}
+						</h3>
+						<h3 key={movie.id} className='director'>
+							{movie.director}
+						</h3>
+						<p key={movie.id} className='movieDescription'>
+							{movie.description}
+						</p>
+						<h6 key={movie.id} className='rtScore'>
+							{movie.rt_score}
+						</h6>
+					</section>
+				))} */}
 		</div>
 	);
 }
