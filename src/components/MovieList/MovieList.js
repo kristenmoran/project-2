74a4554fad78ea8c911381cddcde531c6270f458
kristenmoran <kristen.moran30@gmail.com>
2020-04-29 20:02:59 -0400

    added cards to the movie list

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './MovieList.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Poster from '../../imgs/posters/myneighbortotoro.jpg';
 
 function MovieList({ films }) {
 	if (films.length === 0) {
@@ -8,19 +13,29 @@ function MovieList({ films }) {
 	}
 
 	return (
-		<div className='movieListContainer container-fluid'>
-			{films &&
-				films.map((movie) => (
-					<div key={movie.id} className='filmsList row justify-content-center'>
-						<Link
-							to={`/films/${movie.title}`}
-							className='films col col-sm-12 col-lg-6 text-center'
-							key={movie.id}>
-							{movie.title}
-						</Link>
-					</div>
-				))}
-		</div>
+		<Container>
+			<Row>
+				<div className='movieListContainer'>
+					{films &&
+						films.map((movie) => (
+							<Col sm={true} key={movie.id} className='mb-3'>
+								<Card
+									style={{ width: '18rem', display: 'inline-block' }}
+									key={movie.id}
+									className='filmsList text-center'>
+									<Card.Img variant='top' src={Poster} alt={movie.title} />
+									<Link
+										to={`/films/${movie.title}`}
+										className='films'
+										key={movie.id}>
+										{movie.title}
+									</Link>
+								</Card>
+							</Col>
+						))}
+				</div>
+			</Row>
+		</Container>
 	);
 }
 
