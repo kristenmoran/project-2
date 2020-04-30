94dc15b61421fa72f0d29068c341c13fd7c784eb
kristenmoran <kristen.moran30@gmail.com>
2020-04-29 23:24:40 -0400

    added bootstrap styling to movie component

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,24 +1,38 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Movie.css';
 import Poster from '../../imgs/posters/myneighbortotoro.jpg';
 
 function Movie({ films, match }) {
 	let movie = films.find(({ title }) => title === match.params.title);
 
 	return (
-		<div className='movieContainer'>
-			<img className='img-fluid' src={Poster} alt='movie poster' />
-			{movie ? (
-				<section key={movie.id} className='descriptionContainer'>
-					<h1 className='movieTitle'>{movie.title}</h1>
-					<h3 className='releaseDate'>{movie.release_date}</h3>
-					<h3 className='director'>{movie.director}</h3>
-					<p className='movieDescription'>{movie.description}</p>
-					<h6 className='rtScore'>{movie.rt_score}</h6>
-				</section>
-			) : (
-				<p>Loading</p>
-			)}
-		</div>
+		<Container>
+			<div className='movieContainer'>
+				<Row>
+					<div className='col-md-8'>
+						<img className='img-fluid' src={Poster} alt='movie poster' />
+					</div>
+					{movie ? (
+						<Col>
+							<div key={movie.id} className='descriptionContainer col-md-4'>
+								<h1 className='movieTitle'>{movie.title}</h1>
+								<h3 className='releaseDate'>{movie.release_date}</h3>
+								<h3 className='director'>{movie.director}</h3>
+								<p className='movieDescription'>{movie.description}</p>
+								<h6 className='rtScore'>
+									Rotten Tomatoes Score: {movie.rt_score}
+								</h6>
+							</div>
+						</Col>
+					) : (
+						<p>Loading</p>
+					)}
+				</Row>
+			</div>
+		</Container>
 	);
 }
 
