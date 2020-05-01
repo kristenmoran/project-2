96b1ff7f160fdbe6064ac2244058a98528635d0d
kristenmoran <kristen.moran30@gmail.com>
2020-04-30 21:46:02 -0400

    tweaked code

--- src/components/MovieVideo/MovieVideo.js
+++ src/components/MovieVideo/MovieVideo.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
-import VideoUrls from './VideoUrls'
+import React from 'react';
+import ReactPlayer from 'react-player';
+import VideoUrls from './VideoUrls';
 
-function MovieVideo (props) {
-	let trailer = VideoUrls.find((video) => props.movieId === video.id)
+function MovieVideo(props) {
+	let trailer = VideoUrls.find((video) => props.movieId === video.id);
 
 	if (!trailer) return null;
-	
+
 	return (
 		<div>
 			<ReactPlayer
