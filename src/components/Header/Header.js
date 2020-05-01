3be82f12e6f0313ee80096bf80442a84add2ad60
kristenmoran <kristen.moran30@gmail.com>
2020-05-01 17:28:35 -0400

    burger menu added without functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,20 +9,30 @@ function Header() {
 				<Link className='appName' to='/'>
 					Studio Ghibli Database
 				</Link>
-				<Link className='homepage' to='/'>
-					Home
-				</Link>
-				<Link className='moviesButton' to='/films'>
-					Movies
-				</Link>
-				<Link className='contact' to='_blank'>
-					Search
-				</Link>
-				<div className='burger'>
+				<div className='navLinks'>
+					<ul className='navList'>
+						<li>
+							<Link className='homepage' to='/'>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link className='moviesButton' to='/films'>
+								Movies
+							</Link>
+						</li>
+						<li>
+							<Link className='contact' to='_blank'>
+								Search
+							</Link>
+						</li>
+					</ul>
+				</div>
+				<Link to='/' className='burger'>
 					<div className='line1'></div>
 					<div className='line2'></div>
 					<div className='line3'></div>
-				</div>
+				</Link>
 			</nav>
 		</header>
 	);
